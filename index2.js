20b34f0447ff3979c7c5a217544566837f678972
Agent-A451 <61901857+Agent-A451@users.noreply.github.com>
2020-03-26 16:33:39 +0100

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,14 @@
+var myname = "Alphonsus Oko";
+    console.log(myname);
+    var mycourses = ["Html", "Css", "Js", "Nodejs", "Design", "Csharp", "Go"];
+    console.log(mycourses);
+
+    for (let i = 0; i < 200; i++) {
+        if (mycourses % 2 == 1 && i % 2 == 1) {
+            console.log(i);
+        }
+        else {
+            console.log(i += 1);
+        }
+    }
+
